c0b821358fe1c75763ad3eeb989f88bd753a31aa
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-04-04 17:01:19 +0100

    displayed each item

--- collection.php
+++ collection.php
@@ -5,51 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--    <link rel="stylesheet" href="css/normalize.css">-->
 <!--    <link href="css/all.min.css" rel="stylesheet" type="text/css">-->
-    <link rel="stylesheet" href="collectionpractice.css">
+    <link rel="stylesheet" href="collection/collectionpractice.css">
     <title>Record Collection</title>
 </head>
 
 <body>
 <h1>Record Collection</h1>
-<section class="collection">
-<div class="recordcontainer">
-    <img class='vinyl' src="vinyl.png" alt="vinyl">
-    <div class="recordinfo">
+<!--<section class="collection">-->
+<!--<div class="recordcontainer">-->
+<!--    <div class="recordinfo">-->
 <?php
 
 require_once 'db.php';
-
-require_once 'db.php';
 $db = 'recorddb';
-$records = fetchAllRecordsData(connectToDB('recorddb'));
+$records = fetchAllRecordsData(connectToDB($db));
 
-echo '<pre>';
-print_r($records);
-echo '</pre>';
 
-function displayRecord(array $records): string
+function displayRecords(array $records): void
 {
     foreach ($records as $record) {
-        return '<p>' . $record['record'] . '</p>' .
-            '<p>' . $record['artist'] . '</p>' .
-            '<p>' . $record['date'] . '</p>' .
-            '<p>' . $record['record_label'] . '</p>' .
-            '<p>' . $record['song'] . '</p>';
+        echo
+            "<img class='vinyl' src='images/vinyl.png'>" .
+            '<h2>' . $record['name'] . '</h2>' .
+            '<p> By ' . $record['artist'] . '</p>' .
+            '<p> Released in ' . $record['year'] . ' by '. $record['record_label'] . '</p>' .
+            '<p>Featuring ' . $record['song'] . '</p>' .
+            '<img src=' . 'images/' . $record['img_name'] . '>';
     }
 }
 
-//echo displayRecord($records);
-
+displayRecords($records);
 
 ?>
-    </div>
-    <div class="images">
-        <img src='images/londoncalling Small.png'>
-    </div>
+</div>
+
 </div>
 </section>
 </body>
 </html>
-
-
-
